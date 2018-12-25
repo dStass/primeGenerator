8df1aa073d296443996ce36adea56ab699fc6e73
dStass <dstiasny@gmail.com>
2018-12-25 11:20:53 +1100

    Initial setup - eclipse project

---
+++ src/Application.java
@@ -0,0 +1,11 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		int digits = Integer.parseInt(args[0]); // first argument = number of digits of prime
+		
+		System.out.println("digits="+digits);
+
+	}
+
+}
